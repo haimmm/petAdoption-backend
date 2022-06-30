1f9a1125164bfe9fab8d5c5a539b20b2bd435b7e
Haim <haim881994@gmail.com>
2022-06-30 17:20:57 +0300

    mongo fix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
 }))
